e6b3f6fcc4d7d3f3c670e63c7618ff24a6b891b6
kmp-wizard-updater <kmp-wizard-updater-no-reply@jetbrains.com>
2026-09-11 12:05:59 +0200

    publish template

---
+++ app/shared/test@android/PlatformAndroidTest.kt
@@ -0,0 +1,11 @@
+package org.example.project
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class PlatformAndroidTest {
+    @Test
+    fun `platform name is reported`() {
+        assertTrue(getPlatform().name.isNotBlank())
+    }
+}
