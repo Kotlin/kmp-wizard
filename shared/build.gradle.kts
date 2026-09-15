7ab3a3c82d28656173ffb108c4dd00b43aa89939
kmp-wizard-updater <kmp-wizard-updater-no-reply@jetbrains.com>
2026-09-15 14:57:56 +0000

    update template

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,11 +23,13 @@ kotlin {
     
     js {
         browser()
+        binaries.executable()
     }
     
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
+        binaries.executable()
     }
     
     android {
