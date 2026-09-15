df7d581a019d5f084376e15d6682226611d1fdc8
kmp-wizard-updater <kmp-wizard-updater-no-reply@jetbrains.com>
2026-09-15 14:58:12 +0000

    update template

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.13.0-dev-4394
+set kotlin_cli_version=0.13.0-dev-4399
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=ebec3550308a07ba85f9cd4fb82aa7d49859da1209dd070510196cba8eeb4e05
+set kotlin_cli_sha256=1ca22bffee73b785258cbc1b889e1ffbfecb53e19d26a18bc40ce487a68aea61
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
